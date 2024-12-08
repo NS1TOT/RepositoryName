9ecb7066f16157acc846f5f9f94bb5c6595f1bfe
NS1TOT <sergeeva.ao@outlook.com>
2024-12-08 18:17:07 +0300

    Update Lab_3_1.py
    Замена 1 лабораторной

--- Lab_3_1.py
+++ Lab_3_1.py
@@ -1,11 +1,15 @@
-# TODO Напишите функцию find_common_participants
-def find_common_participants(list_1, list_2, symbol=','):
-    set_1 = set(list_1.split(symbol))
-    set_2 = set(list_2.split(symbol))
-    common_list = sorted(list(set_1.intersection(set_2)))
-    return common_list
+# TODO Напишите функцию для поиска индекса товара
+def find_item_i(item_list,item):
+    if item in item_list:
+        return items_list.index(item)
+    else:
+        return None
 
-participants_first_group = "Иванов|Петров|Сидоров"
-participants_second_group = "Петров|Сидоров|Смирнов"
+items_list = ['яблоко', 'банан', 'апельсин', 'груша', 'киви', 'банан']
 
-print(find_common_participants(participants_first_group, participants_second_group, symbol='|'))
+for find_item in ['банан', 'груша', 'виногад']:
+    index_item =  find_item_i(items_list,find_item)
+    if index_item is not None:
+        print(f"Первое вхождение товара '{find_item}' имеет индекс {index_item}.")
+    else:
+        print(f"Товар '{find_item}' не найден в списке.")
